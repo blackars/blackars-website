81e514248a3b7fd067cbc278b29bace3c6cfbfc1
blackars <castillo.brayan@correounivalle.edu.co>
2025-04-21 15:57:54 -0500

    Update linkeidn URL: www.linkedin.com/in/blackars

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   github: "https://github.com/blackars",
-  linkedin: "https://www.linkedin.com/in/andr%C3%A9s-mesa-54620b175/",
+  linkedin: "https://www.linkedin.com/in/blackars",
   discord: "",
   instagram: "https://www.instagram.com/_blackars/",
   twitter: "https://twitter.com/_arsblack",
