b8f18e8e9a9beb2a3756de2bf5febb828291bd4d
blackars <castillo.brayan@correounivalle.edu.co>
2025-09-06 22:12:37 -0500

    cambiando enlace canal youtube

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@ export const LINKS = {
   instagram: "https://www.instagram.com/_blackars/",
   twitter: "https://twitter.com/_arsblack",
   kaggle: "https://www.kaggle.com/blackars",
-  youtube: "https://www.youtube.com/@_blackars",
+  youtube: "https://www.youtube.com/@blackars3",
 };
 
 export const loaderAnimation = [
